331ed5b0dd847c25edb86a37410c238e42da2082
Titux Metal (DEV) <github@lgdweb.fr>
2025-06-29 07:02:09 +0200

    fix(contact): update Contact component text and styling for improved clarity and user engagement

--- apps/frontend/src/components/sections/Contact.tsx
+++ apps/frontend/src/components/sections/Contact.tsx
@@ -2,13 +2,17 @@ import { FaEnvelope, FaPhone } from 'react-icons/fa6'
 
 export const Contact = () => {
   return (
-    <section id='contact' className='my-6 rounded-lg bg-zinc-800 py-12'>
-      <div className='container mx-auto px-4'>
+    <section id='contact' className='section'>
+      <div className='container mx-auto p-4 sm:p-6'>
         <h2 className='mb-6 text-2xl font-bold text-zinc-100'>Contactez-moi</h2>
-        <div className='p-6'>
-          <p className='mb-4 text-zinc-300'>
-            Intéressé par cette Peugeot 206 CC ? Prenez contact pour organiser une visite ou un
-            essai dans le secteur de <strong>Cernay</strong>. Je peux me déplacer un peu si besoin.
+        <div className='flex flex-col gap-4'>
+          <p className='text-zinc-300'>
+            Elle vous laisse sous le charme ? Vous trouvez qu'elle a du potentiel ?
+          </p>
+          <p className='mb-2 text-zinc-300'>
+            C'est le moment de me contacter, laissez-moi un message sur mon répondeur ou directement
+            par email pour organiser une visite ou un essai, elle se trouve dans le secteur de{' '}
+            <strong>Cernay</strong>.
           </p>
           <p className='text-zinc-200'>
             <FaPhone className='icon' aria-label='Téléphone' /> Téléphone :{' '}
@@ -23,7 +27,7 @@ export const Contact = () => {
             </a>
           </p>
           <p className='mt-4 text-zinc-400'>
-            Disponible en semaine ou le week-end pour répondre à vos questions.
+            Je suis disponible en semaine ou le week-end pour répondre à vos questions.
           </p>
         </div>
       </div>
